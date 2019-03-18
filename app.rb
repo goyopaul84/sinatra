8c9fc97cba27ee65e7742c7596acdde6dc5e413f
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-18 15:32:30 +0000

    Cat image sent to route /cat using HTML

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+    "FUCKING YEAH!"
+end
+
+get '/Greg' do
+  "OH MY WORD!"
+end
+
+get '/Harry' do
+  "WHOAAAAAAA"
+end
+
+get '/Makers' do
+  "SLIME!!"
+end
+
+get '/bants' do
+  "gibraltar es britanico"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+   </div>"
+end
