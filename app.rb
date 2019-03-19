491808827d4c8aa3d9729d2444e639bac834eddd
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-19 16:00:03 +0000

    Form functionality added

--- app.rb
+++ app.rb
@@ -24,6 +24,12 @@ get '/bants' do
   "gibraltar es britanico"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
